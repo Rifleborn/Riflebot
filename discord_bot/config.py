b2091b9fb2a7216812971490682f7b0439f7cec9
Roman <butcherimpg@gmail.com>
2021-09-04 23:57:17 +0300

    adding database some another changes

--- discord_bot/config.py
+++ discord_bot/config.py
@@ -1,4 +1,6 @@
 # DICTIONARY
 settings = {
-    'TOKEN' : 'NzUzMjc2MzU4MjU1NDQzOTg5.X1j1Rw.dKNqUMS7vk4KAMw3Bken7OlZHqQ'
+    'TOKEN' : 'NzUzMjc2MzU4MjU1NDQzOTg5.X1j1Rw.dKNqUMS7vk4KAMw3Bken7OlZHqQ',
+    #without ' ' because its channel id
+    'TEST_CHANNEL' : 883369387439046676
 }
--- discord_bot/discord_main.py
+++ discord_bot/discord_main.py
@@ -1,28 +1,65 @@
 #comment - ctrl + /
-#=================
+#========================================================================================================
 #tasks
 #1. додати автовидачу ролі боту при доєднанні на сервер
 #2. зробити відправку повідомлення по таймінгу 14сек 88 мілісекунд (типу того)
 #3. повідомлення в конкретний канал
 #4. як відправляти емодзі
+#5. вивід в лог чат
+#6. команду help
 import discord
 
-#setting import
 from config import settings
 from discord.ext import commands
 from discord.utils import find
+import sqlite3
+import os
 
-bot = commands.Bot(command_prefix='/')
+bot = commands.Bot(command_prefix='/', intents=discord.Intents.all(), help_command=None)
 
+# connecting to database
+try:
+    sqlite_connection = sqlite3.connect('users.db')
+    cursor = sqlite_connection.cursor()
+    print("Database created and successfully connected to SQLite")
+
+    sqlite_select_query = "select sqlite_version();"
+    cursor.execute(sqlite_select_query)
+    record = cursor.fetchall()
+    print("Database version SQLite: ", record)
+    #cursor.close()
+
+except sqlite3.Error as error:
+    print("Error with connection to sqlite", error)
+
+# finally:
+#     if (sqlite_connection):
+#         sqlite_connection.close()
+#         print("Соединение с SQLite закрыто")
+
+#========================================================================================
+#event when bot is online
 @bot.event
 async def on_ready():
     print('We have logged in as {0.user}\n'.format(bot))
+    #getting channel by id (using development mod in discord)
+    channel = bot.get_channel(settings['TEST_CHANNEL'])
+    await channel.send(f'Ready to engage')
+
+@bot.command()
+async def help(context):
+    await context.send("Custom help command")
 
 @bot.event
 async def on_message(message):
     # if this message author is our bot(client)
     if message.author == bot.user:
         return
+
+    #insert data (using ? for safety inserting without SQL injections
+    cursor.execute('INSERT INTO users_messages VALUES(?, ?, ?)', (str(message.author), str(message.content), 'ff'))
+
+    sqlite_connection.commit()
 
     print(f'User ID: {message.author}\nMessage: {message.content}\n')
     # start for checking player commands
